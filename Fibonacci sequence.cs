33781d2f6eada14ee55698e784799da6801fdfaf
rakash-rasheed <75601534+rakash-rasheed@users.noreply.github.com>
2024-07-11 04:33:59 +0500

    Add files via upload

---
+++ Fibonacci sequence.cs
@@ -0,0 +1,38 @@
+﻿// C# code to implement the approach
+using System;
+
+class GFG
+{
+
+	// Method to calculate the nth fibonacci number
+	public static int fibonacci_numbers(int n)
+	{
+		if (n == 0)
+		{
+			return 0;
+		}
+		else if (n == 1)
+		{
+			return 1;
+		}
+		else
+		{
+			return fibonacci_numbers(n - 2) + fibonacci_numbers(n - 1);
+		}
+	}
+
+
+	// Driver Code
+	public static void Main(string[] args)
+	{
+		int n = 7;
+		for (int i = 0; i < n; i++)
+		{
+			// Function call
+			Console.Write(fibonacci_numbers(i) + " ");
+		}
+	}
+}
+
+
+// This code is contributed by phasing17
