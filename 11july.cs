16ee383d3db9098bb7855dd0fb31a566385e504c
rakash-rasheed <75601534+rakash-rasheed@users.noreply.github.com>
2024-07-11 06:33:34 -0400

    Add files via upload

---
+++ 11july.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        class Book
+        {
+            String title, Author;
+            int BookId;
+            void Display_info()
+            {
+
+            }
+        }
+
+        class Person
+        {
+            String Name;
+            int age, PersonID;
+        }
+        class Library
+        {
+            String LibraryName;
+            int Library_ID;
+            List<Library> LibraryList;
+
+            void add_book(Book book) { }
+            void add_book(Book book_id) { }
+            void view_book() { }
+
+        }
+        static void Main(string[] args)
+        {
+       
+        }
+    }
+}
