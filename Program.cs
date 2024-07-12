41e407a9ba64746609cdd98ec0b79dcce0ca1559
rakash-rasheed <75601534+rakash-rasheed@users.noreply.github.com>
2024-07-11 20:38:45 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,36 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApplication4
+class NonRepeatingChar
 {
-    class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Enter a String:");
+        string word = Console.ReadLine();
+        char firstNonRepeatingChar = '\0';
+        int[] charCount = new int[256]; // Assuming ASCII characters
+
+        // Count the occurrences of each character
+        foreach (char ch in word)
         {
-            Console.Write("Rakash");
-            even(10);  //call even number function    
-            Console.Write(sum(5, 9));//sum of two numbers
+            charCount[ch]++;
         }
-        //function for even number 
-        static void even(int number)
+
+        // Find the first non-repeating character
+        foreach (char ch in word)
         {
-            for (int i = 0; i <= number; i++)
+            if (charCount[ch] == 1)
             {
-                if (i % 2 == 0)
-                    Console.WriteLine(i.ToString());
+                firstNonRepeatingChar = ch;
+                break;
             }
         }
 
-        //sum of 2 numbers
-        static int sum(int number1, int number2)
+        if (firstNonRepeatingChar != '\0')
         {
-            int total = number1 + number2;
-            return total;
-            Console.ReadLine();
-
+            Console.WriteLine("The first character which is not repeating is: " + firstNonRepeatingChar);
+        }
+        else
+        {
+            Console.WriteLine("There is no non-repeating character in the input.");
         }
     }
+    static void mergeArrays(int[] arr1, int[] arr2, int n1, int n2, int[] arr3)
+    {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        // traverse the arr1 and insert its element in arr3
+        while (i < n1)
+        {
+            arr3[k++] = arr1[i++];
+        }
+
+        // now traverse arr2 and insert in arr3
+        while (j < n2)
+        {
+            arr3[k++] = arr2[j++];
+        }
+
+        // sort the whole array arr3
+        Array.Sort(arr3);
+    }
+
 }
