fcc7debfef430be3f8f7f044bf2421448d5c5d7d
rakash-rasheed <75601534+rakash-rasheed@users.noreply.github.com>
2024-07-12 06:59:33 -0400

    Add files via upload

---
+++ Quiz.cs
@@ -0,0 +1,20 @@
+﻿int[] arr = { 5, 10, 15 };
+int min = arr[0];
+int max = arr[0];
+for (int i = 1; i < arr.Length; i++)
+{
+    if (arr[i] < min)
+        min = arr[i];
+}
+for (int i = 1; i < arr.Length; i++)
+{
+    if (arr[i] > max)
+        max = arr[i];
+}
+int count = 0;
+for (int i = min; i<max; i++)
+{
+    count++;
+}
+Console.WriteLine(count-2);
+
