1041d584870cfa820bdea6b18e45a116032391f0
rakash-rasheed <75601534+rakash-rasheed@users.noreply.github.com>
2024-07-12 06:40:14 -0400

    Add files via upload

---
+++ 12july.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MyApp;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array = { -2, 10, 4 } ;
+        int largest = 0;
+        int small = int.MaxValue;
+        int l = array.Length;
+        int[] temp = new int[l];
+        int diff ;
+        int sum;
+        for (int j = 0; j < array.Length; j++)
+        {
+            if (array[j] > largest)
+            {
+                largest = array[j];
+            }
+        }
+        for (int k = 0; k < array.Length; k++)
+        {
+            if (array[k] < small)
+            {
+                small = array[k];
+            }
+        }
+        
+        
+        for (int i = 0; i < l; i++)
+        {
+            if (array[i] > small || array[i] < largest)
+            {
+                diff = largest- array[i];
+                sum = array[i] - small;
+               
+                
+;                
+            }
+
+        }
+        Console.WriteLine(sum-diff);
+
+    }
+}
+
